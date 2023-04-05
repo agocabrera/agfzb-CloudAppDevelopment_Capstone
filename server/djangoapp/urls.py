98527510b3efbf10e37cbb0914912e9e7445c4fd
Agustín <agocabrera@gmail.com>
2023-04-05 05:40:26 -0300

    Capstone project week 1

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -4,25 +4,12 @@ from django.conf import settings
 from . import views
 
 app_name = 'djangoapp'
-urlpatterns = [
-    # route is a string contains a URL pattern
-    # view refers to the view function
-    # name the URL
-
-    # path for about view
-
-    # path for contact us view
-
-    # path for registration
-
-    # path for login
-
-    # path for logout
-
-    path(route='', view=views.get_dealerships, name='index'),
-
-    # path for dealer reviews view
-
-    # path for add a review view
-
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns = [path(route='about/', view=views.about, name='about'),
+               path(route='contact/', view=views.contact, name='contact'),
+               path(route='registration/', view=views.registration_request, name='registration'),
+               path(route='login/', view=views.login_request, name='login'),
+               path(route='logout/', view=views.logout_request, name='logout'),
+               path(route='', view=views.get_dealerships, name='index'),
+               # path for dealer reviews view
+               # path for add a review view
+               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
